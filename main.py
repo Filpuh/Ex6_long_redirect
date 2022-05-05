99769c0dcafde5018f83edc70fdb0a0f43301618
Filpuh <Dagoniks@gmail.com>
2022-05-05 10:50:10 +0300

    Ex6 fix

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ response = requests.get("https://playground.learnqa.ru/api/long_redirect")
 response_redirect = response.history
 count_response_redirect = len(response_redirect)
 print(f"Количесвто редиректов: {count_response_redirect}")
-print(f"Итоговый статус: {response.status_code}")
+print(f"Итоговый URL: {response.url}")
